bc2b1da727b45026b07f0bd0eae109e5e328791c
sun <329937626@qq.com>
2020-11-26 13:33:50 +0800

    VGCoreAuto.tlh

--- src/import/CorelDrawImporter.cpp
+++ src/import/CorelDrawImporter.cpp
@@ -10,16 +10,10 @@
 #include <QDialog>
 #include <QMessageBox>
 
-#ifdef _DEBUG
-// If you are not building 32-bit Debug target, update this include path, so
-// the IntelliSense manages to load the generated header file for the type library.
-#include <LaserController.dir\Release\VGCoreAuto.tlh>
-#else
 #import "libid:95E23C91-BC5A-49F3-8CD1-1FC515597048" version("12.0") \
       rename("GetCommandLine", "VGGetCommandLine") \
       rename("CopyFile", "VGCopyFile") \
       rename("FindWindow", "VGFindWindow")
-#endif
 
 CorelDrawImporter::CorelDrawImporter(QWidget* parentWnd, QObject* parent)
     : Importer(parentWnd, parent)
